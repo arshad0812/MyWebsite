52fd6b6b2ec8003e39704b2864218523ea04573b
arshad0812 <adarshadaashish@gmail.com>
2025-02-25 19:18:40 +0530

    Second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,18 @@ for (let i = 0, len = revealDelayElements.length; i < len; i++) {
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
+
+
+// Back to top
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
